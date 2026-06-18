562669f8316fa34aa5aec447a4e941f47f9f573b
michael <michael@aco.de>
2026-06-18 16:00:26 +0200

    ref: refactor of engine in file: src/chef/operations/Fletcher64Checksum.ts

--- src/chef/operations/Fletcher64Checksum.ts
+++ src/chef/operations/Fletcher64Checksum.ts
@@ -11,7 +11,7 @@
  * -----------------------------------------------------------------------------
  */
 
-import { Operation } from "../Operation";
+import { Operation, AnyInput } from "../Operation";
 import Utils from "../Utils";
 
 /**
@@ -40,23 +40,24 @@ export class Fletcher64Checksum extends Operation {
    * @param {Object[]} args
    * @returns {string}
    */
-  run(input: any, _args: any[]): any {
+  run(input: ArrayBuffer, _args: unknown[]): AnyInput {
     let a = 0,
       b = 0;
+    let view: DataView;
     if (ArrayBuffer.isView(input)) {
-      input = new DataView(input.buffer, input.byteOffset, input.byteLength);
+      view = new DataView(input.buffer, input.byteOffset, input.byteLength);
     } else {
-      input = new DataView(input);
+      view = new DataView(input);
     }
 
-    for (let i = 0; i < input.byteLength - 3; i += 4) {
-      a = (a + input.getUint32(i, true)) % 0xffffffff;
+    for (let i = 0; i < view.byteLength - 3; i += 4) {
+      a = (a + view.getUint32(i, true)) % 0xffffffff;
       b = (b + a) % 0xffffffff;
     }
-    if (input.byteLength % 4 !== 0) {
+    if (view.byteLength % 4 !== 0) {
       let lastValue = 0;
-      for (let i = 0; i < input.byteLength % 4; i++) {
-        lastValue = (lastValue << 8) | input.getUint8(input.byteLength - 1 - i);
+      for (let i = 0; i < view.byteLength % 4; i++) {
+        lastValue = (lastValue << 8) | view.getUint8(view.byteLength - 1 - i);
       }
       a = (a + lastValue) % 0xffffffff;
       b = (b + a) % 0xffffffff;
